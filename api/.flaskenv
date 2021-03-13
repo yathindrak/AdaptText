98340f57b945043d7a4e1a029bc9e5ef41b8e34b
Yathindra Kodithuwakku <yathindra,rawya@pearson.com>
2021-03-13 21:12:34 +0530

    Add prometheus

--- api/.flaskenv
+++ api/.flaskenv
@@ -1,2 +1,3 @@
 FLASK_APP=api.py
 FLASK_ENV=development
+DEBUG_METRICS=false
--- api/api.py
+++ api/api.py
@@ -3,6 +3,7 @@ from os import environ
 from flask import Flask
 import flask_cors
 import logging
+from prometheus_flask_exporter import PrometheusMetrics
 
 from .routes.prediction import prediction_routes
 from .commands import init_database, add_user
@@ -14,6 +15,9 @@ from .routes.task import task_routes
 cors = flask_cors.CORS()
 
 app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0.3')
 
 logging.basicConfig(filename='error.log',level=logging.DEBUG)
 
@@ -37,5 +41,12 @@ app.register_blueprint(auth_routes, url_prefix='/api')
 app.register_blueprint(task_routes, url_prefix='/api')
 app.register_blueprint(prediction_routes, url_prefix='/api')
 
+# provide app's version and deploy environment/config name to set a gauge metric
+# register_metrics(app, app_version="v0.1.2", app_config="development")
+
+# Plug metrics WSGI app to your main app with dispatcher
+# dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
 if __name__ == '__main__':
     app.run(debug=True)
+    # run_simple(hostname="0.0.0.0", port=5000, application=dispatcher)
